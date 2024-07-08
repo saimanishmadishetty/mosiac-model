84f76c81819c9be9b4eb955880e9492bb9c74c21
saimanishmadishetty <168367986+saimanishmadishetty@users.noreply.github.com>
2024-07-08 19:28:29 +0530

    Update processor.py

--- processor.py
+++ processor.py
@@ -1,42 +1,21 @@
 import numpy as np
-from PIL import Image, ImageDraw, ImageFont
+import pandas as pd
+from PIL import Image
 from io import BytesIO
 import base64
-
-def pre_process(input):
-    image_data = base64.b64decode(input)
+def pre_process(input_text):
+    image_data = base64.b64decode(input_text)
     # Open the image and convert to RGB
     original_image = Image.open(BytesIO(image_data))
-    # Convert the image to a tensor
-    image = original_image.resize((224, 224))
-    # Convert the image to a numpy array and normalize it
-    image_np = np.array(image)
-    image_np = image_np.astype(np.uint8)
-    # Convert to list
-    image_list = image_np.tolist()
-    return image_list
-def post_process(input):
-    predictions=input
-    boxes = predictions['detection_boxes']
-    scores = predictions['detection_scores']
-    classes = predictions['detection_classes']
-    num_detections = int(predictions['num_detections'])
-    
-    filtered_boxes = []
-    filtered_scores = []
-    filtered_classes = []
-    
-    for i in range(num_detections):
-        if scores[i] > 0.5:
-            filtered_boxes.append(boxes[i])
-            filtered_scores.append(scores[i])
-            filtered_classes.append(classes[i])
-    
-    filtered_predictions = {
-        'detection_boxes': filtered_boxes,
-        'detection_scores': filtered_scores,
-        'detection_classes': filtered_classes,
-        'num_detections': len(filtered_scores)
-    }
-    
-    return filtered_predictions
+    image = original_image.resize((32, 32), Image.Resampling.LANCZOS)
+    # Convert the image to grayscale
+    image = image.convert('L')
+    # Convert the image to a numpy array
+    img_array = np.array(image)
+    # Initialize an 8x8 matrix to hold the counts of "on" pixels
+    blocks = img_array.reshape(8, 4, 8, 4).sum(axis=(1, 3))
+    # Normalize counts to fit in the range 0 to 16
+    blocks = (blocks / blocks.max() * 16).astype(int)
+    # Convert the numpy array to a Python list
+    blocks_list = blocks.reshape(1, -1).tolist()[0]
+    return blocks_list
