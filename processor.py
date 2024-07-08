3f972bd69dd6054c5c5c7bbeaac89bff4bfef1b6
saimanishmadishetty <168367986+saimanishmadishetty@users.noreply.github.com>
2024-07-08 18:24:07 +0530

    Update processor.py

--- processor.py
+++ processor.py
@@ -1,42 +1,37 @@
 import numpy as np
-from PIL import Image, ImageDraw, ImageFont
+from PIL import Image
 from io import BytesIO
 import base64
 
-def pre_process(input):
-    #print("Input to pre_transform (first 20 characters):", input[:20])
-    image_data = base64.b64decode(input)
-    # Open the image and convert to RGB
-    original_image = Image.open(BytesIO(image_data))
-    # Convert the image to a tensor
-    image = original_image.resize((224, 224))
-    # Convert the image to a numpy array and normalize it
-    image_np = np.array(image)
-    image_np = image_np.astype(np.uint8)
-    # Convert to list
-    image_list = image_np.tolist()
-    return image_list
 
-def post_process(input):
-    predictions = input
-    boxes = np.array(predictions['detection_boxes'])
-    scores = np.array(predictions['detection_scores'])
-    classes = np.array(predictions['detection_classes'])
-    num_detections = int(predictions['num_detections'])
-    
-    # Create a boolean mask where scores are greater than 0.5
-    mask = scores > 0.5
-    
-    # Use the mask to filter the boxes, scores, and classes
-    filtered_boxes = boxes[mask]
-    filtered_scores = scores[mask]
-    filtered_classes = classes[mask]
-    
-    filtered_predictions = {
-        'detection_boxes': filtered_boxes.tolist(),
-        'detection_scores': filtered_scores.tolist(),
-        'detection_classes': filtered_classes.tolist(),
-        'num_detections': len(filtered_scores)
-    }
-    
-    return filtered_predictions
+def softmax(x):
+   """Compute softmax values for each set of scores in x."""
+   e_x = np.exp(x - np.max(x))
+   return e_x / e_x.sum(axis=0)
+
+
+def pre_process(input):
+   # Displaying initial part of the input for debugging
+   img_height = 180
+   img_width = 180
+   # Decode the input, load as an image, resize, and convert to a NumPy array
+   img_array = np.array(Image.open(BytesIO(base64.b64decode(input))).resize((img_height, img_width)))
+   transformed_input = img_array.tolist()
+   # Displaying initial part of the transformed input for debugging
+   return transformed_input
+
+
+def post_process(input_scores):
+   # Displaying initial part of the input scores for debugging
+   class_names = ['daisy', 'dandelion', 'roses', 'sunflowers', 'tulips']
+   # Apply softmax to the input scores
+   probabilities = softmax(np.array(input_scores))
+   # Find the index of the maximum score/probability
+   class_index = np.argmax(probabilities)
+   class_name = class_names[class_index]
+   confidence = 100 * probabilities[class_index]
+   # Convert confidence to float for consistency
+   confidence_value = float(confidence)
+   processed_response = (class_name, confidence_value)
+   # Displaying processed response for debugging
+   return processed_response
